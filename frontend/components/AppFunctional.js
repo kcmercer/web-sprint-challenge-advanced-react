c108fcc5c1e5e14224602dbe854a760b2652d673
Kenneth Mercer <kmercer5439@gmail.com>
2022-02-11 15:13:13 -0600

    adjusted everything for functional app

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,170 +1,171 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 
 const initialState = {
     x: 2,
     y: 2,
     steps: 0,
-    email: ''
-}
-
-const initialInput = {
-  email: ''
+    email: '',
+    message: ''
 }
 
 export default function AppFunctional(props) {
   const [ value, setValue ] = useState(initialState)
-  const [ emailInput, setEmailInput ] = useState(initialInput)
 
-  const moveSquare = e => {
+  const xDown = () => {
+    value.y <= 3 && value.y >= 2 ? 
+    setValue({
+      ...value, 
+      y: value.y - 1, 
+      steps: value.steps + 1
+    }) :
+    setValue({
+      ...value, 
+      y: value.y === 3 ? 3 : value.y, 
+      message: "You can't go up"
+    })
+  }
 
-    if (e.target.id === 'up') {
-      setValue({
-        ...value,
-        steps: value.steps + 1,
-        y: Math.max(1, value.y - 1)
-      })
-    } else if (e.target.id === 'left') {
-      setValue({
-        ...value,
-        steps: value.steps + 1,
-        x: Math.max(1, value.x - 1)
-      })
-    } else if (e.target.id === 'right') {
-      setValue({
-        ...value,
-        steps: value.steps + 1,
-        x: Math.min(3, value.x + 1)
-      })
-    } else if (e.target.id === 'down') {
-      setValue({
-        ...value,
-        steps: value.steps + 1,
-        y: Math.min(3, value.y + 1)
-      })
-    } else if (e.target.id === 'reset') {
+  const yDown = () => {
+    value.x <= 2 && value.x >= 1 ? 
+    setValue({
+      ...value, 
+      x: value.x + 1, 
+      steps: value.steps + 1}) :
+    setValue({
+      ...value, 
+      x: value.x === 3 ? 3 : value.x, 
+      message: "You can't go right"})
+  }
+
+  const xUp = () => {
+    value.y <= 2 && value.y >= 1 ? 
+    setValue({
+      ...value, 
+      y: value.y + 1, 
+      steps: value.steps + 1}) :
+    setValue({
+      ...value, 
+      y: value.y === 3 ? 3 : value.y, 
+      message: "You can't go down"})
+  }
+
+  const yUp = () => {
+    value.x <= 3 && value.x >= 2 ? 
+    setValue({
+      ...value, 
+      x: value.x - 1, 
+      steps: value.steps + 1
+    }) :
+    setValue({
+      ...value, 
+      x: value.x === 3 ? 3 : value.x, 
+      message: "You can't go left"})
+  }
+
+  const fullReset = () => {
       const input = document.querySelector('#email')
+      const messageBox = document.querySelector('#message')
       input.value = ''
+      messageBox.value = ''
 
-      setValue({
-        ...value,
-        x: 2,
-        y: 2,
-        steps: 0,
-        email: '',
-      })
-    } else {
-      console.log('What is going on..?')
-    }
+    setValue({
+      ...value,
+      x:2,
+      y:2,
+      steps:0,
+      email: "",
+      message:''
+    })
   }
 
   const onChange = e => {
-    setEmailInput({
+    setValue({
+      ...value,
       email: e.target.value
     })
   }
 
   const onSubmit = e => {
     e.preventDefault();
-
-    setValue({
-      ...value,
-      email: emailInput.email
-    })
-
-    const messageBox = document.querySelector('#message')
-    // console.log(messageBox)
+    const input = document.querySelector('#email')
+    input.value = ''
 
     axios.post('http://localhost:9000/api/result', value)
     .then(resp => {
-      messageBox.innerHTML = resp.data.message
+      setValue({
+        ...value,
+        message: resp.data.message
+      })
     })
     .catch(error => {
-      console.log(error.message)
-      messageBox.innerHTML = error.message
+      console.log(error)
+      if (value.email === 'foo@bar.baz') {
+        setValue({
+          ...value,
+          message: 'foo@bar.baz failure #71'
+        })
+      } else if (value.email === '') {
+        setValue({
+          ...value,
+          message: 'Ouch: email is required'
+        })
+      } else {
+        setValue({
+          ...value,
+          message: 'Ouch: email must be a valid email'
+        })
+      }
     })
   }
-
-  useEffect(() => {
-    const grid = document.querySelectorAll('div.square')
-    // const messageBox = document.querySelector('#message')
-
-    Array.from(grid).map(el => {
-      el.classList.remove('active'),
-      el.innerHTML = ''
-    })
-
-    // axios.post('http://localhost:9000/api/result', this.state)
-    // .then(resp => {
-    //   messageBox.innerHTML = resp.data.message
-    // })
-    // .catch(error => {
-    //   console.log(error.message)
-    //   messageBox.innerHTML = error.message
-    // })
-
-    if (value.x === 1 && value.y === 1) {
-      grid[0].classList.add('active')
-      grid[0].innerHTML = 'B'
-    } else if (value.x === 1 && value.y === 2) {
-      grid[3].classList.add('active')
-      grid[3].innerHTML = 'B'
-    } else if (value.x === 1 && value.y === 3) {
-      grid[6].classList.add('active')
-      grid[6].innerHTML = 'B'
-    } else if (value.x === 2 && value.y === 1) {
-      grid[1].classList.add('active')
-      grid[1].innerHTML = 'B'
-    } else if (value.x === 2 && value.y === 2) {
-      grid[4].classList.add('active')
-      grid[4].innerHTML = 'B'
-    } else if (value.x === 2 && value.y === 3) {
-      grid[7].classList.add('active')
-      grid[7].innerHTML = 'B'
-    } else if (value.x === 3 && value.y === 1) {
-      grid[2].classList.add('active')
-      grid[2].innerHTML = 'B'
-    } else if (value.x === 3 && value.y === 2) {
-      grid[5].classList.add('active')
-      grid[5].innerHTML = 'B'
-    } else if (value.x === 3 && value.y === 3) {
-      grid[8].classList.add('active')
-      grid[8].innerHTML = 'B'
-    } else {
-      console.log('Not sure how this happened...')
-    }
-  })
-
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">Coordinates ({value.x}, {value.y})</h3>
-        <h3 id="steps">You moved {value.steps} times</h3>
+        <h3 id="steps">You moved {value.steps} {value.steps !== 1 ? 'times' : 'time'}</h3>
       </div>
 
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        <div className={value.x === 1 && value.y === 1 ? 'square active' : 'square'}>
+          {value.x === 1 && value.y === 1 ? 'B' : ''}
+        </div>
+        <div className={value.x === 2 && value.y === 1 ? 'square active' : 'square'}>
+          {value.x === 2 && value.y === 1 ? 'B' : ''}
+        </div>
+        <div className={value.x === 3 && value.y === 1 ? 'square active' : 'square'}>
+          {value.x === 3 && value.y === 1 ? 'B' : ''}
+        </div>
+        <div className={value.x === 1 && value.y === 2 ? 'square active' : 'square'}>
+          {value.x === 1 && value.y === 2 ? 'B' : ''}
+        </div>
+        <div className={value.x === 2 && value.y === 2 ? 'square active' : 'square'}>
+          {value.x === 2 && value.y === 2 ? 'B' : ''}
+        </div>
+        <div className={value.x === 3 && value.y === 2 ? 'square active' : 'square'}>
+          {value.x === 3 && value.y === 2 ? 'B' : ''}
+        </div>
+        <div className={value.x === 1 && value.y === 3 ? 'square active' : 'square'}>
+          {value.x === 1 && value.y === 3 ? 'B' : ''}
+        </div>
+        <div className={value.x === 2 && value.y === 3 ? 'square active' : 'square'}>
+          {value.x === 2 && value.y === 3 ? 'B' : ''}
+        </div>
+        <div className={value.x === 3 && value.y === 3 ? 'square active' : 'square'}>
+          {value.x === 3 && value.y === 3 ? 'B' : ''}
+        </div>
       </div>
 
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{value.message}</h3>
       </div>
 
       <div id="keypad">
-        <button onClick={moveSquare} id="left">LEFT</button>
-        <button onClick={moveSquare} id="up">UP</button>
-        <button onClick={moveSquare} id="right">RIGHT</button>
-        <button onClick={moveSquare} id="down">DOWN</button>
-        <button onClick={moveSquare} id="reset">reset</button>
+        <button onClick={yUp} id="left">LEFT</button>
+        <button onClick={xDown} id="up">UP</button>
+        <button onClick={yDown} id="right">RIGHT</button>
+        <button onClick={xUp} id="down">DOWN</button>
+        <button onClick={fullReset} id="reset">reset</button>
       </div>
 
       <form>
